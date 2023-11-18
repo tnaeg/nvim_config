248bee471a2aad6a01d905d292593ab4c43e2597
Mikael Johansson <Mikael.johansson001@gmail.com>
2023-11-18 21:24:48 +0100

    Cleanup

--- lua/lsp/symbol_outline.lua
+++
@@ -1,7 +0,0 @@
-local status_ok, outline = pcall(require, "symbols-outline")
-if not status_ok then
-  vim.notify("No plugin called symbols-outline", vim.log.levels.ERROR)
-  return
-end
-
-outline.setup()
